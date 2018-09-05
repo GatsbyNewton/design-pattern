5dfed382ceff08cb68b50e20d93cc98a180dcd8c
GatsbyNewton <wangzmking@163.com>
2018-09-06 00:36:33 +0800

    简单工厂模式模板

--- src/main/java/edu/wzm/creator/simple_factory/template/OperationFactory.java
+++ src/main/java/edu/wzm/creator/simple_factory/template/OperationFactory.java
@@ -18,16 +18,4 @@ public class OperationFactory {
 
         return operator;
     }
-
-    public static void main(String[] args){
-        Operator add = OperationFactory.createOperator("+");
-        add.setNumA(1);
-        add.setNumB(2);
-        System.out.println("numA + numB = " + add.getResult());
-
-        Operator sub = OperationFactory.createOperator("-");
-        sub.setNumA(1);
-        sub.setNumB(2);
-        System.out.println("numA - numB = " + sub.getResult());
-    }
 }
