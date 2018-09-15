2510c8a0e134b44defaa6d1248fb9d3031d6a21e
GatsbyNewton <wangzmking@163.com>
2018-09-15 18:27:20 +0800

    原型模式示例：浅复制

---
+++ src/main/java/edu/wzm/creator/prototype/shallow/Resume.java
@@ -0,0 +1,53 @@
+package edu.wzm.creator.prototype.shallow;
+
+public class Resume implements Cloneable {
+    private String name;
+    private int age;
+    private Experience experience;
+
+    public Resume(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setExperience(String date, String company) {
+        this.experience = new Experience(date, company);
+    }
+
+    public Resume(String name, int age, String date, String company) {
+        this.name = name;
+        this.age = age;
+        this.experience = new Experience(date, company);
+    }
+
+    public void setExperienceInfo(String date, String company) {
+        this.experience.setDate(date);
+        this.experience.setCompany(company);
+    }
+
+    public Experience getExperience() {
+        return experience;
+    }
+
+    @Override
+    protected Resume clone() throws CloneNotSupportedException {
+        Resume resume = null;
+        try {
+            resume = (Resume) super.clone();
+        }catch (CloneNotSupportedException cnse){
+            throw new RuntimeException();
+        }
+
+        return resume;
+    }
+
+    @Override
+    public String toString() {
+        return "Resume{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", experience={date=" + experience.getDate() +
+                ", company=" + experience.getCompany() +
+                "}}";
+    }
+}
