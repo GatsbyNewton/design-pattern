261056ef140bf99af0ab00fff95be488556c4ea1
GatsbyNewton <wangzmking@163.com>
2018-10-05 16:25:50 +0800

    观察者模式与委托

--- src/main/java/edu/wzm/action/observer/delegate/Event.java
+++ src/main/java/edu/wzm/action/observer/delegate/Event.java
@@ -5,10 +5,10 @@ import java.lang.reflect.Method;
 import java.util.Objects;
 
 public class Event {
-    private Object object;
-    private String methodName;
-    private Object[] params;
-    private Class[] paramTypes;
+    private Object object;          // 被执行的方法所属对象
+    private String methodName;      // 被执行的方法名
+    private Object[] params;        // 被执行的方法参数列表
+    private Class[] paramTypes;     // 被执行的方法参数类型列表
 
     public Event(Object object, String methodName, Object... params) {
         this.object = object;
@@ -17,6 +17,10 @@ public class Event {
         contractParamType(params);
     }
 
+    /**
+     * 根据被执行方法的参数，获取参数对应的类型
+     * @param params
+     */
     private void contractParamType(Object[] params){
         int paramNum = params.length;
         this.paramTypes = new Class[paramNum];
@@ -25,6 +29,12 @@ public class Event {
         }
     }
 
+    /**
+     * 调用被执行的方法
+     * @throws NoSuchMethodException
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     */
     public void invoke()throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
         /* 判断是否存在这个函数 */
         Method method = object.getClass().getMethod(methodName, paramTypes);
--- src/main/java/edu/wzm/action/observer/delegate/EventHandler.java
+++ src/main/java/edu/wzm/action/observer/delegate/EventHandler.java
@@ -11,10 +11,22 @@ public class EventHandler {
         this.events = new ArrayList<>();
     }
 
+    /**
+     * 添加被执行方法的事件
+     * @param object 被执行方法所属的对象
+     * @param methodName 被执行的方法名
+     * @param params 被执行的方法参数列表
+     */
     public void addEvent(Object object, String methodName, Object... params){
         events.add(new Event(object, methodName, params));
     }
 
+    /**
+     * 调用被执行的方法
+     * @throws NoSuchMethodException
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     */
     public void notifyALL()throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
         for (Event event : events){
             event.invoke();
